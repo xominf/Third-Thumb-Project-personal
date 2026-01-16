d39f4c61243e8050d769b9446f99d4a9c4c7b32b
xominf <101848349+xominf@users.noreply.github.com>
2026-01-15 22:16:15 -0600

    Refactor debug functions for clarity and consistency
    Updated debugStart and debugMenuFunction to improve clarity and consistency in sensor output messages.

--- picoCode/ThirdThumbProgramBeta/debugFunction.ino
+++ picoCode/ThirdThumbProgramBeta/debugFunction.ino
@@ -1,38 +1,46 @@
 #include "mainH.h"
 
-//starts the debugStart() function
-//This function runs the debugging part of the code. This allows the testing of the servos and the reading of the EMG/ECG sensor outputs.
-void debugStart(){
+// -----------------------------------------------------------------------------
+// debugStart()
+//
+// This function listens for serial input.  When the user sends '9', a debug
+// menu is displayed.  The options allow testing the servos and reading the
+// pressure sensors in real time.
+void debugStart() {
   if (Serial.available() > 0) {
-    if ((int)Serial.read() == 57) {
+    if ((int)Serial.read() == 57) { // '9'
       debugMenu = true;
       Serial.println("");
       Serial.println(">0: Test openCloseServo");
       Serial.println(">1: Test tiltServo");
-      Serial.println(">2: Show tiltServoSensor output values");
-      Serial.println(">3: Show openCloseServoSensor output values");
+      Serial.println(">2: Show tilt pressure sensor values");
+      Serial.println(">3: Show open/close pressure sensor values");
       Serial.println("");
-      Serial.println(">Type \'9\' at any time to open this menu again");
+      Serial.println(">Type '9' at any time to open this menu again");
       debugMenuFunction();
     }
   }
   resetServoPos();
 }
 
-//This function starts the loop of the debugging code.
+// -----------------------------------------------------------------------------
+// debugMenuFunction()
+//
+// Main loop for the debug menu.  This function blocks until the user exits.
 void debugMenuFunction() {
   while (true) {
     while (Serial.available() > 0) {
       switch (Serial.read()) {
-        case 57:  // number 9
+        case 57:  // number '9'
           Serial.println(">0: Test openCloseServo");
           Serial.println(">1: Test tiltServo");
-          Serial.println(">2: Show tiltServoSensor output values");
-          Serial.println(">3: Show openCloseServoSensor output values");
-          Serial.println(">Type \'9\' at any time to open this menu again");
+          Serial.println(">2: Show tilt pressure sensor values");
+          Serial.println(">3: Show open/close pressure sensor values");
+          Serial.println(">Type '9' at any time to open this menu again");
           break;
 
-        case 48:  // number 0
+        case 48:  // number '0'
+          // Sweep the open/close servo from 0 to 180 and back
           for (openCloseServoPos = 0; openCloseServoPos <= 180; openCloseServoPos++) {
             digitalWrite(LED_BUILTIN, HIGH);
             Serial.print(">openCloseServo: ");
@@ -52,7 +60,8 @@ void debugMenuFunction() {
           Serial.println(">Done");
           break;
 
-        case 49:  // number 1
+        case 49:  // number '1'
+          // Sweep the tilt servo from 0 to 180 and back
           for (tiltServoPos = 0; tiltServoPos <= 180; tiltServoPos++) {
             digitalWrite(LED_BUILTIN, HIGH);
             Serial.print(">tiltServo: ");
@@ -71,35 +80,37 @@ void debugMenuFunction() {
           }
           Serial.println(">Done");
           break;
-        
-        case 50:    //number 2
-          while (true){
-            Serial.println(analogRead(tiltServoECGSensorPin));
 
-            if (Serial.available() > 0){
-              if(((int)Serial.read() == 99) || ((int)Serial.read() == 67)){ //99 = c 67 = C
+        case 50:    // number '2'
+          // Continuously print tilt pressure sensor values until 'c' or 'C' is pressed
+          while (true) {
+            Serial.println(analogRead(tiltPressureSensorPin));
+            if (Serial.available() > 0) {
+              int c = Serial.read();
+              if (c == 99 || c == 67) { // 'c' or 'C'
                 break;
               }
             }
-
             delay(15);
           }
+          break;
 
-        case 51:    //number 3
-          while (true){
-            Serial.println(analogRead(openCloseServoECGSensorPin));
-
-            if (Serial.available() > 0){
-              if(((int)Serial.read() == 99) || ((int)Serial.read() == 67)){ //99 = c 67 = C
+        case 51:    // number '3'
+          // Continuously print open/close pressure sensor values until 'c' or 'C' is pressed
+          while (true) {
+            Serial.println(analogRead(openClosePressureSensorPin));
+            if (Serial.available() > 0) {
+              int c = Serial.read();
+              if (c == 99 || c == 67) { // 'c' or 'C'
                 break;
               }
             }
-
             delay(15);
           }
+          break;
 
         default:
-          Serial.println(">");
+          Serial.println(">\n");
           break;
       }
     }
